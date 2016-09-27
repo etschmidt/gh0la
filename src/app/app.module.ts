fe87a36a53f2e5efcfac742c07188073021621b6
Ethan T Schmidt <ethan.t.schmidt@gmail.com>
2016-09-27 12:00:10 -0700

    adds items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { StoriesComponent } from './stories/stories.component';
+import { FooterComponent } from './footer/footer.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    StoriesComponent,
+    FooterComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
